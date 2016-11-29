d491dfc3e64832dc0d470a774d7d929f35130e0c
Clara Ferrando <clara.ferrando@gmail.com>
2016-11-28 23:59:55 -0300

    Moved to Bootstrap 3.3.7 (glyph). Added Employee details page

--- company/urls.py
+++ company/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^customers$', views.customers, name='customers'),
     url(r'^orders', views.orders, name='orders'),
     url(r'^employees', views.employees, name='employees'),
+    url(r'^employee/(?P<pk>\d+)/$',	views.employee_detail,	name='employee_detail'),
 ]
--- company/views.py
+++ company/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Customers, Orders, Employees
 
+
 # Create your views here.
 def customers(request):
     customers = Customers.objects.all()
@@ -9,12 +10,20 @@ def customers(request):
     return render(request,"company/customers.html", context)
     #return HttpResponse("Hello, world. You're at the polls index.")
 
+
 def orders(request):
     orders = Orders.objects.all()
     context = {'orders': orders}
     return render(request,"company/orders.html", context)
 
+
 def employees(request):
     employees = Employees.objects.all()
     context = {'employees': employees}
     return render(request,"company/employees.html", context)
+
+
+def employee_detail(request, pk):
+    employee = Employees.objects.filter(pk=pk)
+    employee_rec = employee[0] if len(employee) else None
+    return render(request, 'company/employee_detail.html', {'employee': employee_rec})
